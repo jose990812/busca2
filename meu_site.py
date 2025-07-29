2933fab02616372b6f97bebfa52a179d4ddb4e20
jose990812 <ailton.bbb.brh@gmail.com>
2025-07-29 11:33:10 -0300

    terceiro comit

--- meu_site.py
+++ meu_site.py
@@ -52,5 +52,8 @@ def buscar():
     except Exception as e:
         return jsonify({"erro": f"Erro ao carregar planilha: {str(e)}"})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
